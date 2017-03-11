7d319247f903b2603e6c941b6042a8dff01ee0c9
ahong211 <ahong211@gmail.com>
2017-03-10 21:19:29 -0500

    Test Picasso api with example image

--- app/src/main/java/com/example/albert/popularmovies/MainActivity.java
+++ app/src/main/java/com/example/albert/popularmovies/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.albert.popularmovies;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
+import android.widget.ImageView;
+
+import com.squareup.picasso.Picasso;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +13,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ImageView imageView = (ImageView) findViewById(R.id.imageView);
+
+        Picasso.with(this)
+                .load("http://cdn-1.famouslogos.us/images/android-logo.jpg")
+                .into(imageView);
     }
 
     @Override
