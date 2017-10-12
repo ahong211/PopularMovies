9eb7b4f098b57ba64bae2a17f1ee9c0b6546a7a5
ahong211 <ahong211@gmail.com>
2017-10-11 22:26:39 -0700

    Add AsyncTask with Thread.sleep

--- app/src/main/java/com/albert/popularmoviesredux/MainActivity.java
+++ app/src/main/java/com/albert/popularmoviesredux/MainActivity.java
@@ -1,24 +1,56 @@
 package com.albert.popularmoviesredux;
 
-import android.support.v7.app.AppCompatActivity;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
 
 public class MainActivity extends AppCompatActivity {
 
+    TextView showText;
     ImageView showExampleImage;
+    FetchMovieTask mFetchMovieTask = new FetchMovieTask();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        showExampleImage = (ImageView) findViewById(R.id.iv_main_activity);
+        initilizeLayouts();
+        mFetchMovieTask.execute();
 
         Glide.with(this)
                 .load("http://www.icon2s.com/wp-content/uploads/2014/04/black-white-android-film-reel.png")
                 .into(showExampleImage);
+    }
+
+    private void initilizeLayouts() {
+        showText = (TextView) findViewById(R.id.tv_show_text);
+        showExampleImage = (ImageView) findViewById(R.id.iv_main_activity);
+    }
+
+    public class FetchMovieTask extends AsyncTask<Void, Void, String> {
+
+        @Override
+        protected String doInBackground(Void... params) {
+
+            try {
+                Thread.sleep(3000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+            return "Done with sleeping threads";
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+
+            showText.setText(s);
+        }
     }
 }
