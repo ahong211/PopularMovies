ea9500077c446c2153879e33b1f90e672aebc6e9
ahong211 <ahong211@gmail.com>
2017-10-11 20:55:34 -0700

    Add dependencies and show example image with Glide

--- app/src/main/java/com/albert/popularmoviesredux/MainActivity.java
+++ app/src/main/java/com/albert/popularmoviesredux/MainActivity.java
@@ -2,12 +2,23 @@ package com.albert.popularmoviesredux;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
 
 public class MainActivity extends AppCompatActivity {
+
+    ImageView showExampleImage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        showExampleImage = (ImageView) findViewById(R.id.iv_main_activity);
+
+        Glide.with(this)
+                .load("http://www.icon2s.com/wp-content/uploads/2014/04/black-white-android-film-reel.png")
+                .into(showExampleImage);
     }
 }
---
+++ app/src/main/java/com/albert/popularmoviesredux/MovieInfo.java
@@ -0,0 +1,14 @@
+package com.albert.popularmoviesredux;
+
+/**
+ * Created by Albert on 10/11/17.
+ */
+
+public class MovieInfo {
+
+    public MovieInfo() {
+
+    }
+
+
+}
