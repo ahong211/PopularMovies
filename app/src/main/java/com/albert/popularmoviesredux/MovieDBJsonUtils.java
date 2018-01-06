db79994216451bd3ce28a8b0cd99600bd556e0d1
ahong211 <ahong211@gmail.com>
2018-01-06 15:00:50 -0800

    Add json util class, modified MovieInfo model, update gitignore

---
+++ app/src/main/java/com/albert/popularmoviesredux/MovieDBJsonUtils.java
@@ -0,0 +1,51 @@
+package com.albert.popularmoviesredux;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+/**
+ * Created by Albert on 1/6/18.
+ */
+
+public class MovieDBJsonUtils {
+    public static MovieInfo[] getMovieDBStringsFromJson(String movieInfoJsonStr) throws JSONException {
+
+        final String MDB_RESULTS = "results";
+        final String MDB_ORIGINAL_TITLE = "original_title";
+        final String MDB_POSTER_PATH = "poster_path";
+        final String MDB_OVERVIEW = "overview";
+        final String MDB_VOTE_AVERAGE = "vote_average";
+        final String MDB_RELEASE_DATE = "release_date";
+
+        MovieInfo[] parsedMovieData = null;
+
+        JSONObject movieJson = new JSONObject(movieInfoJsonStr);
+
+        JSONArray movieArray = movieJson.getJSONArray(MDB_RESULTS);
+
+        parsedMovieData = new MovieInfo[movieArray.length()];
+
+        for (int i = 0; i < movieArray.length(); i++) {
+            MovieInfo movieInfo;
+            String originalTitle;
+            String posterPath;
+            String overview;
+            String voteAverage;
+            String releaseDate;
+
+            JSONObject movieInfoItem = movieArray.getJSONObject(i);
+            originalTitle = movieInfoItem.getString(MDB_ORIGINAL_TITLE);
+            posterPath = movieInfoItem.getString(MDB_POSTER_PATH);
+            overview = movieInfoItem.getString(MDB_OVERVIEW);
+            voteAverage = movieInfoItem.getString(MDB_VOTE_AVERAGE);
+            releaseDate = movieInfoItem.getString(MDB_RELEASE_DATE);
+
+            movieInfo = new MovieInfo(originalTitle, overview, posterPath, voteAverage, releaseDate);
+
+            parsedMovieData[i] = movieInfo;
+        }
+
+        return parsedMovieData;
+    }
+}
--- app/src/main/java/com/albert/popularmoviesredux/MovieInfo.java
+++ app/src/main/java/com/albert/popularmoviesredux/MovieInfo.java
@@ -6,28 +6,37 @@ package com.albert.popularmoviesredux;
 
 public class MovieInfo {
 
-    String title;
-    String description;
+    private String originalTitle;
+    private String overview;
+    private String posterPath;
+    private String voteAverage;
+    private String releaseDate;
 
-    public MovieInfo(String title, String description) {
-        this.title = title;
-        this.description = description;
+    public MovieInfo(String originalTitle, String overview, String posterPath, String voteAverage, String releaseDate) {
+        this.originalTitle = originalTitle;
+        this.overview = overview;
+        this.posterPath = posterPath;
+        this.voteAverage = voteAverage;
+        this.releaseDate = releaseDate;
     }
 
-    public String getTitle() {
-        return title;
+    public String getOriginalTitle() {
+        return originalTitle;
     }
 
-    public void setTitle(String title) {
-        this.title = title;
+    public String getOverview() {
+        return overview;
     }
 
-    public String getDescription() {
-        return description;
+    public String getPosterPath() {
+        return posterPath;
     }
 
-    public void setDescription(String description) {
-        this.description = description;
+    public String getVoteAverage() {
+        return voteAverage;
     }
 
+    public String getReleaseDate() {
+        return releaseDate;
+    }
 }
